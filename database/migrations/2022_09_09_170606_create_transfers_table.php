a49652a82a799cb774af75d53ff74672da7fd8cd
Engineer Baqir Amini <baqir.amini01@gmail.com>
2022-09-09 21:45:35 +0430

    Create table transfers.

---
+++ database/migrations/2022_09_09_170606_create_transfers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->increments('tran_id');
+            $table->integer('currency_code')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('from_cust_id')->unsigned();
+            $table->integer('to_cust_id')->unsigned();
+            $table->decimal('amount_transferred', 12, 2);
+            $table->integer('status', 1)->default(0);
+            $table->foreign('currency_code')->references('currency_code')->on('currencies')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('user_id')->references('user_id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('from_cust_id')->references('cust_id')->on('customers')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('to_cust_id')->references('cust_id')->on('customers')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
