0f29f1e32a0e3208c88383f0bd448ced4502d2b3
Engineer Baqir Amini <baqir.amini01@gmail.com>
2022-09-23 11:48:36 +0430

    Setup React web template with Laravel.

---
+++ back-end/build/precache-manifest.4db565af4ce2b13cc25d7be0fdb6b6a3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec3e4224a76ac9235de6",
+    "url": "./resources/static/js/main.ec3e4224.chunk.js"
+  },
+  {
+    "revision": "cc5f614cd43c09c09a8d",
+    "url": "./resources/static/js/1.cc5f614c.chunk.js"
+  },
+  {
+    "revision": "24086322f374ae91e134",
+    "url": "./resources/static/js/runtime~main.24086322.js"
+  },
+  {
+    "revision": "16a9baeff8d65181ffae63248a365a34",
+    "url": "./resources/static/media/team-4.16a9baef.jpg"
+  },
+  {
+    "revision": "a539dfc606a5972269f12b8de55a458f",
+    "url": "./resources/static/media/team-3.a539dfc6.jpg"
+  },
+  {
+    "revision": "90c40d0c92e7916a53cad8934a8fe170",
+    "url": "./resources/static/media/team-2.90c40d0c.jpg"
+  },
+  {
+    "revision": "0e461794078d703ca99147d87f562e4e",
+    "url": "./resources/static/media/team-1.0e461794.jpg"
+  },
+  {
+    "revision": "3dc0055972d67e851dec8ad06df8ba38",
+    "url": "./resources/static/media/logo-atlassian.3dc00559.svg"
+  },
+  {
+    "revision": "9a05c08ac513debf06aaae52550a70aa",
+    "url": "./resources/static/media/logo-slack.9a05c08a.svg"
+  },
+  {
+    "revision": "b0a5df30d111b61f0bf446edb8af80a8",
+    "url": "./resources/static/media/logo-spotify.b0a5df30.svg"
+  },
+  {
+    "revision": "4731441a6ff1afa4b9faff6377d7013e",
+    "url": "./resources/static/media/logo-invision.4731441a.svg"
+  },
+  {
+    "revision": "c0c969935d58c7f7d2f1d2b9e82e9968",
+    "url": "./resources/static/media/logo-xd.c0c96993.svg"
+  },
+  {
+    "revision": "cb154bf24a4dd036a299e4a51880f7ae",
+    "url": "./resources/static/media/logo-jira.cb154bf2.svg"
+  },
+  {
+    "revision": "e1efc60049237101c8aad446f7e8f284",
+    "url": "./resources/static/media/mastercard.e1efc600.png"
+  },
+  {
+    "revision": "adf903ff428d0029b6bbbe8d3b0cc3ef",
+    "url": "./resources/static/media/logo-asana.adf903ff.svg"
+  },
+  {
+    "revision": "822fcecab7571a77bad2df64edf294be",
+    "url": "./resources/static/media/github.822fceca.svg"
+  },
+  {
+    "revision": "0fc5376fbb9ce047b82762732d4caa37",
+    "url": "./resources/static/media/pattern-tree.0fc5376f.svg"
+  },
+  {
+    "revision": "98edc46df132450fd5a38d389ec38bf4",
+    "url": "./resources/static/media/visa.98edc46d.png"
+  },
+  {
+    "revision": "0c4404672522984ae6363728960701b9",
+    "url": "./resources/static/media/bruce-mars.0c440467.jpg"
+  },
+  {
+    "revision": "20187467e59c2821d337b1a6229574a8",
+    "url": "./resources/static/media/bg-profile.20187467.jpeg"
+  },
+  {
+    "revision": "779cce2b24ad0740119753e8a0b25757",
+    "url": "./resources/static/media/kal-visuals-square.779cce2b.jpg"
+  },
+  {
+    "revision": "a60473a071f57732624ce2b82dc8b038",
+    "url": "./resources/static/media/marie.a60473a0.jpg"
+  },
+  {
+    "revision": "a190088866e3ef8efc4d04342a4216b2",
+    "url": "./resources/static/media/ivana-square.a1900888.jpg"
+  },
+  {
+    "revision": "decbdadf95d853e4474a49e7b1a6197d",
+    "url": "./resources/static/media/home-decor-1.decbdadf.jpg"
+  },
+  {
+    "revision": "249f600cbd9742a97c7a57a3f7118b8b",
+    "url": "./resources/static/media/home-decor-2.249f600c.jpg"
+  },
+  {
+    "revision": "2d98a7aa0729e2262ea4017fed4f0af5",
+    "url": "./resources/static/media/home-decor-3.2d98a7aa.jpg"
+  },
+  {
+    "revision": "69753763f3a27a6f400690fea2b13778",
+    "url": "./resources/static/media/home-decor-4.69753763.jpeg"
+  },
+  {
+    "revision": "307f32fe57c713e172581d7648dbfccd",
+    "url": "./resources/static/media/bg-sign-in-basic.307f32fe.jpeg"
+  },
+  {
+    "revision": "a3fd89f3290db6918f8e14106d4a4431",
+    "url": "./resources/static/media/bg-sign-up-cover.a3fd89f3.jpeg"
+  },
+  {
+    "revision": "aab5525d99f9acf7d0fcd95facb2190c",
+    "url": "./resources/index.html"
+  }
+];
