c4c4f02e1cd275d470a0cce16808ba30b704f2c6
Engineer Baqir Amini <baqir.amini01@gmail.com>
2022-10-15 10:30:07 +0430

    Validate form inputs while adding a new customer.

--- back-end/app/Http/Controllers/APIController.php
+++ back-end/app/Http/Controllers/APIController.php
@@ -4,48 +4,67 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Customer;
+use Validator;
 
 class APIController extends Controller
 {
-      # register new customer
-      public function onCreateCustomer(Request $request)
-      {
-        $customer = new Customer();
-          $customer = new Customer();
-          $customer->first_name = $request->firstName;
-          $customer->last_name = $request->lastName;
-          $customer->father_name = $request->fatherName;
-          $customer->tazkira_ID = $request->tazkiraID;
-          $customer->contact_NO = $request->contactNO;
-          $customer->photo = $request->photo;
-          $customer->email = $request->email;
-          $customer->address = $request->address;
-          if ($customer->save()) {
-              return response()->json(
-                ['message' => 'success']);
-          } else {
-              return response()->json(['message' => 'fail']);
-          }
+  # register new customer
+  public function onCreateCustomer(Request $request)
+  {
+
+    # Validate customers' info
+    $validator = Validator::make($request->all(), [
+      'firstName' => 'required|string|min:3|max:64',
+      'lastName' => 'nullable|string|min:3|max:64',
+      'fatherName' => 'required|string|min:3|max:64',
+      'tazkira_number' => 'required|string|min:13|max:13|unique:customers,tazkira_ID',
+      'contact_number' => 'required|string|min:10|max:16|unique:customers,contact_NO',
+      'email' => 'nullable|string|email|unique:customers,email',
+      'address' => 'required|string|min:16|max:256'
+    ]);
+
+    if (!$validator->fails()) {
+      $customer = new Customer();
+      $customer->first_name = $request->firstName;
+      $customer->last_name = $request->lastName;
+      $customer->father_name = $request->fatherName;
+      $customer->tazkira_ID = $request->tazkira_number;
+      $customer->contact_NO = $request->contact_number;
+      $customer->photo = $request->photo;
+      $customer->email = $request->email;
+      $customer->address = $request->address;
+      if ($customer->save()) {
+        return response()->json(
+          ['status' => 200]
+        );
+      } else {
+        return response()->json(['message' => 'fail']);
       }
+    } else {
+      return response()->json(
+        ['validation_err' => $validator->messages(), 'status' => 0]
+      );
+    }
+  }
 
-      public function onGetCustomer()
-      {
-          $customers = Customer::all();
-          if (isset($customers)) {
-            return response()->json([
-              'customers' => $customers
-            ]);
-          } else {
-            return "not found";
-          }
-      }
+  public function onGetCustomer()
+  {
+    $customers = Customer::all();
+    if (isset($customers)) {
+      return response()->json([
+        'customers' => $customers
+      ]);
+    } else {
+      return "not found";
+    }
+  }
 
 
-    # Fetch customers' info using his/her ID to update
-    public function onEditCustomer($cid)
-    {
-         $customer = Customer::findOrfail($cid);
-         /* $customer->comp_id = $request->compId;
+  # Fetch customers' info using his/her ID to update
+  public function onEditCustomer($cid)
+  {
+    $customer = Customer::findOrfail($cid);
+    /* $customer->comp_id = $request->compId;
          $customer->cust_name = $request->custName;
          $customer->cust_lastname = $request->custLname;
          $customer->cust_phone = $request->custPhone;
@@ -53,27 +72,38 @@ class APIController extends Controller
          $customer->cust_state = $request->custState;
          $customer->cust_addr = $request->custAddress; */
 
-        return response()->json([
-          "status" => 200,
-          "customer" => $customer,
-        ]);
-    }
+    return response()->json([
+      "status" => 200,
+      "customer" => $customer,
+    ]);
+  }
 
-    # Post changes to update a customer
-    public function onPostCustInfo(Request $request, $cid)
-    {
-        $customer = Customer::findOrfail($cid);
-        $customer->first_name = $request->firstName;
-        $customer->last_name = $request->lastName;
-        $customer->father_name = $request->fatherName;
-        $customer->tazkira_ID = $request->tazkiraID;
-        $customer->contact_NO = $request->contactNO;
-        $customer->email = $request->email;
-        $customer->address = $request->address;
-        $customer->update();
-        return response()->json([
-          'status' => 200,
-          'message' => 'The customer updated successfully!'
-        ]);
-    }
+  # Post changes to update a customer
+  public function onPostCustInfo(Request $request, $cid)
+  {
+    $customer = Customer::findOrfail($cid);
+    $customer->first_name = $request->firstName;
+    $customer->last_name = $request->lastName;
+    $customer->father_name = $request->fatherName;
+    $customer->tazkira_ID = $request->tazkiraID;
+    $customer->contact_NO = $request->contactNO;
+    $customer->email = $request->email;
+    $customer->address = $request->address;
+    $customer->update();
+    return response()->json([
+      'status' => 200,
+      'message' => 'The customer updated successfully!'
+    ]);
+  }
+
+  # Delete a customer
+  public function onDeleteCustomer($cid)
+  {
+    $customer = Customer::findOrfail($cid);
+    $customer->delete();
+    return response()->json([
+      'status' => 200,
+      'message' => 'The customer deleted successfully!'
+    ]);
+  }
 }
--- front-end/src/components/NewCustomer.js
+++ front-end/src/components/NewCustomer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
 import axios from 'axios';
+import swal from 'sweetalert';
 
 class NewCustomer extends Component {
 
@@ -8,11 +9,12 @@ class NewCustomer extends Component {
     firstName: '',
     lastName: '',
     fatherName: '',
-    tazkiraID: '',
-    contactNO: '',
+    tazkira_number: '',
+    contact_number: '',
     photo: '',
     email: '',
     address: '',
+    error_list: []
   }
 
   handleInput = (e) => {
@@ -27,21 +29,29 @@ class NewCustomer extends Component {
     let data = JSON.stringify(this.state);
     const res = await axios.post(`http://localhost:8000/api/AddCustomer`, data, { headers: { "Content-Type": "application/json" } });
 
-    if (res.data.message = 'success') {
-      alert('The customer added succefully!');
+    if (res.data.status === 200) {
+      swal({
+        title: "Success!",
+        text: "The customer added!",
+        icon: "success",
+        button: "OK"
+      });
       this.setState({
         firstName: '',
         lastName: '',
         fatherName: '',
-        tazkiraID: '',
-        contactNO: '',
+        tazkira_number: '',
+        contact_number: '',
         photo: '',
         email: '',
-        address: '',
+        address: ''
       });
     } else {
-      alert('Adding customer failed.');
+      this.setState({
+        error_list: res.data.validation_err
+      });
     }
+
   }
 
   render() {
@@ -56,52 +66,56 @@ class NewCustomer extends Component {
                 <form className="forms-sample" onSubmit={this.saveCustomer}>
                   <Form.Group>
                     <label htmlFor="first_name">First Name</label>
-                    <Form.Control type="text" name="firstName" value={this.state.firstName} onChange={this.handleInput} className="form-control" id="cust_first_name" placeholder="First Name" />
+                    <Form.Control type="text" name="firstName" value={this.state.firstName} onChange={this.handleInput} className={(this.state.error_list.firstName ? 'is-invalid' : '')} id="cust_first_name" placeholder="First Name" />
+                    <div className='invalid-feedback'>{this.state.error_list.firstName}</div>
                   </Form.Group>
                   <Form.Group>
                     <label htmlFor="last_name">Last Name</label>
-                    <Form.Control type="text" name="lastName" value={this.state.lastName} onChange={this.handleInput} className="form-control" id="cust_last_name" placeholder="Last Name" />
+                    <Form.Control type="text" name="lastName" value={this.state.lastName} onChange={this.handleInput} className={(this.state.error_list.lastName ? 'is-invalid' : '')} id="cust_last_name" placeholder="Last Name" />
+                    <div className='invalid-feedback'>{this.state.error_list.lastName}</div>
                   </Form.Group>
                   <Form.Group>
                     <label htmlFor="father_name">Father Name</label>
-                    <Form.Control type="text" name="fatherName" value={this.state.fatherName} onChange={this.handleInput} className="form-control" id="cust_father_name" placeholder="Father Name" />
+                    <Form.Control type="text" name="fatherName" value={this.state.fatherName} onChange={this.handleInput} className={(this.state.error_list.fatherName ? 'is-invalid' : '')} id="cust_father_name" placeholder="Father Name" />
+                    <div className='invalid-feedback'>{this.state.error_list.fatherName}</div>
                   </Form.Group>
                   <Form.Group>
                     <label htmlFor="tazkira_ID">Tazkira ID</label>
-                    <Form.Control type="number" name="tazkiraID" value={this.state.tazkiraID} onChange={this.handleInput} className="form-control" id="cust_tazkira_ID" placeholder="Tazkira ID" />
+                    <Form.Control type="number" name="tazkira_number" value={this.state.tazkira_number} onChange={this.handleInput} className={(this.state.error_list.tazkira_number ? 'is-invalid' : '')} id="cust_tazkira_ID" placeholder="Tazkira ID" />
+                    <div className='invalid-feedback'>{this.state.error_list.tazkira_number}</div>
                   </Form.Group>
                   <Form.Group>
                     <label htmlFor="contact_no">Contact NO</label>
-                    <Form.Control type="number" name="contactNO" value={this.state.contactNO} onChange={this.handleInput} className="form-control is-invalid" id="cust_contact_no" placeholder="Contact NO" />
-                    <div className="invalid-feedback">
-                      Please provide a valid Tazkira ID.
-                    </div>
+                    <Form.Control type="number" name="contact_number" value={this.state.contact_number} onChange={this.handleInput} className={(this.state.error_list.contact_number ? 'is-invalid' : '')} id="cust_contact_no" placeholder="Contact NO" />
+                    <div className='invalid-feedback'>{this.state.error_list.contact_number}</div>
                   </Form.Group>
                   <Form.Group>
                     <label>File upload</label>
                     <div className="custom-file">
-                      <Form.Control type="file" name="photo" value={this.state.photo} onChange={this.handleInput} className="form-control visibility-hidden" id="customFileLang" lang="es" />
+                      <Form.Control type="file" name="photo" value={this.state.photo} onChange={this.handleInput} className="visibility-hidden" id="customFileLang" lang="es" />
                       <label className="custom-file-label" htmlFor="customFileLang">Upload image</label>
                     </div>
                   </Form.Group>
                   <Form.Group>
                     <label htmlFor="email">Email address</label>
-                    <Form.Control type="email" name="email" value={this.state.email} onChange={this.handleInput} className="form-control" id="cust_email" placeholder="Email" />
+                    <Form.Control type="email" name="email" value={this.state.email} onChange={this.handleInput} className={(this.state.error_list.email ? 'is-invalid' : '')} id="cust_email" placeholder="Email" />
+                    <div className='invalid-feedback'>{this.state.error_list.email}</div>
                   </Form.Group>
                   {/* <Form.Group>
                       <label htmlFor="gender">Gender</label>
-                      <select className="form-control" id="cust_gender">
+                      <select className={(this.state.isBlank ? 'is-invalid' : '')} id="cust_gender">
                         <option>Male</option>
                         <option>Female</option>
                       </select>
                     </Form.Group> */}
                   {/* <Form.Group>
                       <label htmlFor="exampleInputCity1">City</label>
-                      <Form.Control type="text" className="form-control" id="exampleInputCity1" placeholder="Location" />
+                      <Form.Control type="text" className={(this.state.isBlank ? 'is-invalid' : '')} id="exampleInputCity1" placeholder="Location" />
                     </Form.Group> */}
                   <Form.Group>
                     <label htmlFor="address">Address</label>
-                    <textarea className="form-control" name="address" value={this.state.address} onChange={this.handleInput} id="cust_address" rows="4"></textarea>
+                    <textarea className={"form-control " + (this.state.error_list.address ? 'is-invalid' : '')} name="address" value={this.state.address} onChange={this.handleInput} id="cust_address" rows="4"></textarea>
+                    <div className='invalid-feedback'>{this.state.error_list.address}</div>
                   </Form.Group>
                   <button type="submit" className="btn btn-gradient-primary">Save</button>
                   <button className="btn btn-light">Cancel</button>
